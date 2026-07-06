5823ee67cf5182e1e8a8d299c99826c5281aff30
Akash9719 <78297767+Akash9719@users.noreply.github.com>
2026-07-06 18:52:15 +0530

    Implement load_knowledge function
    Add function to load knowledge from a text file

--- backend/knowledge.py
+++ backend/knowledge.py
@@ -1,1 +1,19 @@
+from pathlib import Path
 
+knowledge = ""
+
+
+def load_knowledge():
+
+    global knowledge
+
+    if knowledge:
+        return knowledge
+
+    path = Path("knowledge.txt")
+
+    with open(path, "r", encoding="utf-8") as file:
+
+        knowledge = file.read()
+
+    return knowledge
